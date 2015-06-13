cd526199e44d6161d4e2e4683c2bb3bde2942a8d
Aleksandar Josić <aleksa_josic@hotmail.com>
2015-06-13 18:02:06 +0200

    Dodatna podešavanja prilikom pretrage vina

--- src/frontend/HomeController.java
+++ src/frontend/HomeController.java
@@ -2,6 +2,8 @@ package frontend;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.Iterator;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -36,7 +38,24 @@ public class HomeController extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("pOCETAK");
 		ArrayList<Wine> wines;
+		ArrayList<Wine>	returnWines = new ArrayList<Wine>();
+		
+	/*	Enumeration<String> e = request.getParameterNames();
+		while(e.hasMoreElements())
+		{
+			String s = e.nextElement();
+			System.out.println("Parametar: " + s);
+		}*/
+		
+		String body = request.getParameter("selectBody");
+		String flavor = request.getParameter("selectFlavor");
+		String type = request.getParameter("selectType");
+		String sugar = request.getParameter("selectSugar");
+		
+	//	System.out.println("Body : " +body);
+	
 		if (request.getParameter("region") != null) {
 			String region = request.getParameter("region");
 			wines = o.winesByProximity(GeoCalculator.getLangLat(region));
@@ -45,7 +64,63 @@ public class HomeController extends HttpServlet {
 			double lng = Double.parseDouble(request.getParameter("lng"));
 			wines = o.winesByProximity(new GeoLocation(lat, lng));
 		}
-		request.setAttribute("wines", wines);
+		
+
+		for(Wine w : wines)
+		{
+			try{
+				if (!body.equals("Any"))
+					if (w.getBody() != null)
+					{
+						if(!w.getBody().equals(body))
+						{
+							continue;
+						}
+					}
+					else
+						continue;
+				if (!flavor.equals("Any"))
+					if (w.getFlavor() != null)
+					{
+						if(!w.getFlavor().equals(flavor))
+						{
+							continue;
+						}
+					}
+					else
+						continue;
+				if (!type.equals("Any"))
+					if (w.getType() != null)
+					{
+						if(!w.getType().equals(type))
+						{
+							continue;
+						}
+					}
+					else
+						continue;
+				if (!sugar.equals("Any"))
+					if (w.getSugar() != null)
+					{
+						if(!w.getSugar().equals(sugar))
+						{
+							continue;
+						}
+					}
+					else
+						continue;
+				
+			//	System.out.println("Prosao");
+				returnWines.add(w);
+			}
+			catch (NullPointerException e1)
+			{
+				System.out.println("Greska !");
+			}
+		}
+	
+		
+		request.setAttribute("wines", returnWines);
 		getServletContext().getRequestDispatcher("/wines.jsp").forward(request, response);
 	}
 
